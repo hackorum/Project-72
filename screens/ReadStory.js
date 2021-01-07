95c44b45996b98a851d4ca45521c8ac00ec551f2
hackorum <70092545+hackorum@users.noreply.github.com>
2021-01-07 22:16:40 +0530

    Add files via upload

---
+++ screens/ReadStory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+export default class ReadStory extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>ReadStory</Text>
+      </View>
+    );
+  }
+}
